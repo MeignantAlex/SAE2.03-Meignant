57d1eb0b141631f4babe177c08a6df5c1799de8d
Meignant <meignant3@mmi-dev.unilim.fr>
2025-04-03 09:39:07 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "meignant3");
+define("DBLOGIN", "meignant3");
+define("DBPWD", "meignant3");
